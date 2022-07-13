68fcc680352bba4827e68856bfd967d6d6e21e67
Griffin <griffinmmatthews@gmail.com>
2022-07-12 20:40:34 -0400

    added suggestion card

--- src/pages/Suggestions.tsx
+++ src/pages/Suggestions.tsx
@@ -40,6 +40,23 @@ function Suggestions() {
                     </div>
                     <Link to="/add-new-feedback"><button className="p-2 button button--purple button--sm">+ Add Feedback</button></Link>
                 </div>
+                <div className="card card--grid">
+                    <div className="card__text">
+                        <h3>There is no feedback yet.</h3>
+                        <p className="p-2">Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
+                    </div>
+                    <div className="card__feature">
+                        <p className="p-2">Feature</p>
+                    </div>
+                    <div className="card__up-vote">
+                        <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 6l4-4 4 4" stroke="#4661E6" stroke-width="2" fill="none" fillRule="evenodd"/></svg>
+                        <p className="p-2">0</p>
+                    </div>
+                    <div className="card__comments">
+                        <svg width="18" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M2.62 16H1.346l.902-.91c.486-.491.79-1.13.872-1.823C1.036 11.887 0 9.89 0 7.794 0 3.928 3.52 0 9.03 0 14.87 0 18 3.615 18 7.455c0 3.866-3.164 7.478-8.97 7.478-1.017 0-2.078-.137-3.025-.388A4.705 4.705 0 012.62 16z" fill="#CDD2EE" fill-rule="nonzero"/></svg>
+                        <p>0</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
