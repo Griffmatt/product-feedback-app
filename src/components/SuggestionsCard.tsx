db69418ceb1263f8dfdce2682c080856b3841e9e
Griffin <griffinmmatthews@gmail.com>
2022-07-25 16:15:29 -0400

    added useMemo for some functions

--- src/components/SuggestionsCard.tsx
+++ src/components/SuggestionsCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 
 import { totalComments } from '../utilities/totalComments'
 
@@ -20,13 +20,13 @@ function Card({suggestion}: props) {
     const [active, setActive] = useState(false)
 
     const dispatch = useDispatch()
-
     const user = useSelector(selectUser)
+
+    const commentsMemo = useMemo(()=> totalComments(suggestion.comments), [suggestion.comments]) 
 
     const handleUpVote = (e: React.MouseEvent) => {
 
         e.stopPropagation();
-        e.preventDefault();
         
         if(user.upvotes.includes(suggestion.id)){
             dispatch(removeUpvote(suggestion.id))
@@ -64,7 +64,7 @@ function Card({suggestion}: props) {
         </div>
         <div className="card__comments">
             <svg width="18" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M2.62 16H1.346l.902-.91c.486-.491.79-1.13.872-1.823C1.036 11.887 0 9.89 0 7.794 0 3.928 3.52 0 9.03 0 14.87 0 18 3.615 18 7.455c0 3.866-3.164 7.478-8.97 7.478-1.017 0-2.078-.137-3.025-.388A4.705 4.705 0 012.62 16z" fill="#CDD2EE" fill-rule="nonzero"/></svg>
-            <p>{totalComments(suggestion.comments)}</p>
+            <p>{commentsMemo}</p>
         </div>
     </div>
   )
